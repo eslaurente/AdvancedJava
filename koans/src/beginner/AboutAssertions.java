8d89af11e4775d749bff7492b4d59637bd7154f0
Emerald Laurente <laurente@pdx.edu>
2014-07-02 10:52:30 -0700

    Finished the first two koans

--- koans/src/beginner/AboutAssertions.java
+++ koans/src/beginner/AboutAssertions.java
@@ -16,32 +16,32 @@ public class AboutAssertions {
 
 	@Koan 
 	public void assertBooleanTrue() {
-		assertTrue(__); // should be true really
+		assertTrue(true); // should be true really
 	}
 
 	@Koan
 	public void assertBooleanFalse() {
-		assertFalse(__); 
+		assertFalse(false);
 	}
 	
 	@Koan
 	public void assertNullObject(){
-		assertNull(__);
+		assertNull(null);
 	}
 	
 	@Koan
 	public void assertNotNullObject(){
-		assertNotNull(null); // anything other than null should pass here...
+		assertNotNull("Not null"); // anything other than null should pass here...
 	}
 
 	@Koan
 	public void assertEqualsUsingExpression(){
-		assertTrue("Hello World!".equals(__));
+		assertTrue("Hello World!".equals("Hello World!"));
 	}
 
 	@Koan
 	public void assertEqualsWithBetterFailureMessage(){
-		assertEquals(1, __);
+		assertEquals(1, 1);
 	}
 
 	@Koan
@@ -49,20 +49,20 @@ public class AboutAssertions {
 		// Generally, when using an assertXXX methods, expectation is on the 
 		// left and it is best practice to use a String for the first arg 
 		// indication what has failed
-		assertEquals("The answer to 'life the universe and everything' should be 42", 42, __);
+		assertEquals("The answer to 'life the universe and everything' should be 42", 42, 42);
 	}
 	
 	@Koan
 	public void assertSameInstance(){
 		Object same				= new Integer(1);
-		Object sameReference	= __;
+		Object sameReference	= same;
 		assertSame(same, sameReference);
 	}
 	
 	@Koan
 	public void assertNotSameInstance(){
 		Integer same           = new Integer(1);
-		Integer sameReference  = same;
+		Integer sameReference  = new Integer(1);
 		assertNotSame(same, sameReference); 
 	}
 }
--- koans/src/beginner/AboutKoans.java
+++ koans/src/beginner/AboutKoans.java
@@ -8,12 +8,12 @@ public class AboutKoans {
 
 	@Koan
 	public void findAboutKoansFile(){
-		fail("delete this line");
+
 	}
 	
 	@Koan
 	public void definitionOfKoanCompletion(){
-		boolean koanIsComplete = false;
+		boolean koanIsComplete = true;
 		if(!koanIsComplete){
 			fail("what if koanIsComplete was true?");
 		}
