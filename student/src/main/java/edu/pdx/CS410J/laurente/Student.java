83a4f8f6cee441eacd22784ebbb61355ae00715a
Emerald Laurente <laurente@pdx.edu>
2014-07-02 14:17:32 -0700

    Two tests in StudentTest.java are passing

--- student/src/main/java/edu/pdx/CS410J/laurente/Student.java
+++ student/src/main/java/edu/pdx/CS410J/laurente/Student.java
@@ -48,6 +48,7 @@ public class Student extends Human {
    * standard out by invoking its <code>toString</code> method.
    */
   public static void main(String[] args) {
-
+      System.err.println("Missing command line arguments");
+      System.exit(1);
   }
 }
