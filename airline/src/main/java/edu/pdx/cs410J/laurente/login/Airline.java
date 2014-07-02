e3a06060ee12a73b3068c41dafca0b4c9f371e97
Emerald Laurente <laurente@pdx.edu>
2014-07-02 11:24:51 -0700

    Added 'login' package and added concrete Airline and Flight classes

---
+++ airline/src/main/java/edu/pdx/cs410J/laurente/login/Airline.java
@@ -0,0 +1,26 @@
+package edu.pdx.cs410J.laurente.login;
+
+import edu.pdx.cs410J.AbstractAirline;
+import edu.pdx.cs410J.AbstractFlight;
+
+import java.util.Collection;
+
+/**
+ * Created by emerald on 7/2/14.
+ */
+public class Airline extends AbstractAirline {
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void addFlight(AbstractFlight abstractFlight) {
+
+    }
+
+    @Override
+    public Collection getFlights() {
+        return null;
+    }
+}
---
+++ airline/src/main/java/edu/pdx/cs410J/laurente/login/Flight.java
@@ -0,0 +1,33 @@
+package edu.pdx.cs410J.laurente.login;
+
+import edu.pdx.cs410J.AbstractFlight;
+
+/**
+ * Created by emerald on 7/2/14.
+ */
+public class Flight extends AbstractFlight {
+    @Override
+    public int getNumber() {
+        return 0;
+    }
+
+    @Override
+    public String getSource() {
+        return null;
+    }
+
+    @Override
+    public String getDepartureString() {
+        return null;
+    }
+
+    @Override
+    public String getDestination() {
+        return null;
+    }
+
+    @Override
+    public String getArrivalString() {
+        return null;
+    }
+}
