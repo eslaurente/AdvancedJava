45101d13ba9442051183c4ae113ca65f2d4d0e8f
Emerald Laurente <laurente@pdx.edu>
2014-07-02 12:43:32 -0700

    Added proj0_student project

---
+++ student/src/test/java/edu/pdx/CS410J/laurente/StudentTest.java
@@ -0,0 +1,31 @@
+package edu.pdx.CS410J.laurente;
+
+import edu.pdx.cs410J.InvokeMainTestCase;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.StringContains.containsString;
+
+/**
+ * JUnit tests for the Student class.  These tests extend <code>InvokeMainTestCase</code>
+ * which allows them to easily invoke the <code>main</code> method of <code>Student</code>.
+ * They also make use of the <a href="http://hamcrest.org/JavaHamcrest/">hamcrest</a>matchers
+ * for more readable assertion statements.
+ */
+public class StudentTest extends InvokeMainTestCase
+{
+
+  @Test
+  public void invokingMainWithNoArgumentsHasExitCodeOf1() {
+    MainMethodResult result = invokeMain(Student.class);
+    assertThat(result.getExitCode(), equalTo(1));
+  }
+
+  @Test
+  public void invokingMainWithNoArgumentsPrintsMissingArgumentsToStandardError() {
+    MainMethodResult result = invokeMain(Student.class);
+    assertThat(result.getErr(), containsString("Missing command line arguments"));
+  }
+
+}
