b8caa7082838ebe53d2b06ad4d34e24c082b0068
Emerald Laurente <laurente@pdx.edu>
2014-07-17 16:31:31 -0700

    Finished AboutLoops koans

--- koans/src/beginner/AboutLoops.java
+++ koans/src/beginner/AboutLoops.java
@@ -14,7 +14,7 @@ public class AboutLoops {
 		for(int i = 0; i < 5; i++) {
 			s += i + " ";
 		}
-		assertEquals(s, __);
+		assertEquals(s, "0 1 2 3 4 ");
 	}
 	
 	@Koan
@@ -23,7 +23,7 @@ public class AboutLoops {
 		for(int i = 0, j = 10; i < 5 && j > 5; i++, j--) {
 			s += i + " " + j + " ";
 		}
-		assertEquals(s, __);
+		assertEquals(s, "0 10 1 9 2 8 3 7 4 6 ");
 	}
 	
 	@Koan
@@ -33,7 +33,7 @@ public class AboutLoops {
 		for(int j : is) {
 			s += "." + j;
 		}
-		assertEquals(s, __);
+		assertEquals(s, "-.1.2.3.4");
 	}
 	
 	@Koan
@@ -42,7 +42,7 @@ public class AboutLoops {
 		while(result < 3) {
 			result++;
 		}
-		assertEquals(result, __);
+		assertEquals(result, 3);
 	}
 	
 	@Koan
@@ -51,7 +51,7 @@ public class AboutLoops {
 		do {
 			result++;
 		} while(false);
-		assertEquals(result, __);
+		assertEquals(result, 1);
 	}
 	
 	@Koan
@@ -64,7 +64,7 @@ public class AboutLoops {
 			}
 			count++;	
 		}
-		assertEquals(count, __);
+		assertEquals(count, 1);
 	}
 	
 	@Koan
@@ -78,7 +78,7 @@ public class AboutLoops {
 				count++;	
 			}
 		}
-		assertEquals(count, __);
+		assertEquals(count, 2);
 	}
 
 	@Koan
@@ -97,7 +97,7 @@ public class AboutLoops {
 		}
 		// What does continue with a label mean? 
 		// What gets executed? Where does the program flow continue?
-		assertEquals(count, __);
+		assertEquals(count, 7);
 	}
 	
 	@Koan
@@ -116,7 +116,7 @@ public class AboutLoops {
 		}
 		// What does break with a label mean? 
 		// What gets executed? Where does the program flow continue?
-		assertEquals(count, __);
+		assertEquals(count, 3);
 	}
 
 }
