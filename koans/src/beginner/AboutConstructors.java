a27947a66ac5e64623813d06c742e16d4a4aeb56
Emerald Laurente <laurente@pdx.edu>
2014-07-19 12:19:10 -0700

    Finished AboutConstructors koan

--- koans/src/beginner/AboutConstructors.java
+++ koans/src/beginner/AboutConstructors.java
@@ -19,7 +19,7 @@ public class AboutConstructors {
 	
 	@Koan
 	public void simpleConstructorOrder(){
-		assertEquals(new B().someString, __);
+		assertEquals(new B().someString, "axg");
 	}
 	
 	class Aa {
@@ -36,7 +36,7 @@ public class AboutConstructors {
 	
 	@Koan
 	public void complexConstructorOrder(){
-		assertEquals(new Bb().someString, __);
+		assertEquals(new Bb().someString, "aBoog");
 	}
 	
 }
