35a774ecc9282558da2f285618e162c028d535dd
Emerald Laurente <laurente@pdx.edu>
2014-07-19 12:15:19 -0700

    Finished AboutConditionals koan

--- koans/src/beginner/AboutConditionals.java
+++ koans/src/beginner/AboutConditionals.java
@@ -14,7 +14,7 @@ public class AboutConditionals {
 		int x = 1;
 		if (true) 
 			x++;
-		assertEquals(x, __);
+		assertEquals(x, 2);
 	}
 	
 	@Koan
@@ -26,7 +26,7 @@ public class AboutConditionals {
 			x++;
 		else
 			x--;
-		assertEquals(x, __);
+		assertEquals(x, 0);
 	}
 	
 	@Koan
@@ -41,7 +41,7 @@ public class AboutConditionals {
 			x = 10;
 		else
 			x--;
-		assertEquals(x, __);
+		assertEquals(x, 10);
 	}
 	
 	@Koan
@@ -55,7 +55,7 @@ public class AboutConditionals {
 			if (otherBooleanCondition) x = 10;
 		else x--;
 		// Where does this else belong to!?
-		assertEquals(x, __);
+		assertEquals(x, 10);
 	}
 	
 	@Koan
@@ -69,7 +69,7 @@ public class AboutConditionals {
 		}
 		// There are different opinions on where the curly braces go...
 		// But as long as you put them here. You avoid problems as seen above.
-		assertEquals(x, __);
+		assertEquals(x, 2);
 	} 
 	
 	@Koan 
@@ -86,7 +86,7 @@ public class AboutConditionals {
 			default:
 				result += "Nothing";
 		}
-		assertEquals(result, __);
+		assertEquals(result, "Basic One");
 	}
 	
 	@Koan 
@@ -101,7 +101,7 @@ public class AboutConditionals {
 			default:
 				result += "Nothing";
 		}
-		assertEquals(result, __);
+		assertEquals(result, "Basic OneTwoNothing");
 	}
 	
 	@Koan 
@@ -116,7 +116,7 @@ public class AboutConditionals {
 			case 2:
 				result += "Two";
 		}
-		assertEquals(result, __);
+		assertEquals(result, "Basic NothingTwo");
 	}
 	
 	@Koan 
@@ -133,7 +133,7 @@ public class AboutConditionals {
 			default:
 				result += "Nothing";
 		}
-		assertEquals(result, __);
+		assertEquals(result, "Basic Nothing");
 	}
 	
 	@Koan 
@@ -150,6 +150,6 @@ public class AboutConditionals {
 			default:
 				result += "Nothing";
 		}
-		assertEquals(result, __);
+		assertEquals(result, "Basic One");
 	}
 }
