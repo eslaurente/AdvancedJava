38e1ebc367e6a1580ba6d47cd6fc658ea8c0663a
Emerald Laurente <laurente@pdx.edu>
2014-07-23 10:11:14 -0700

    Finished AboutOperators koans

--- koans/src/beginner/AboutOperators.java
+++ koans/src/beginner/AboutOperators.java
@@ -9,10 +9,10 @@ public class AboutOperators {
 	@Koan
 	public void plusPlusVariablePlusPlus(){
 		int i = 1;
-		assertEquals(++i, __);
-		assertEquals(i, __);
-		assertEquals(i++, __);
-		assertEquals(i, __);
+		assertEquals(++i, 2);
+		assertEquals(i, 2);
+		assertEquals(i++, 2);
+		assertEquals(i, 3);
 	}
 	
 	@Koan
@@ -22,21 +22,21 @@ public class AboutOperators {
 		// What happens if you replace 'a' with '6' below?
 		// Try this with an IDE like Eclipse...
 		if ( (a < 9 ) || (++i < 8) )  i = i + 1;
-		assertEquals(i, __);
+		assertEquals(i, 2);
 	}
 	
 	@Koan
 	public void fullAnd(){
 		int i = 1;
 		if ( true & (++i < 8) )  i = i + 1;
-		assertEquals(i, __);
+		assertEquals(i, 3);
 	}
 	
 	@Koan
 	public void shortCircuitAnd(){
 		int i = 1;
 		if ( true && (i < -28) )  i = i + 1;
-		assertEquals(i, __);
+		assertEquals(i, 1);
 	}
 	
 	@Koan
@@ -44,7 +44,7 @@ public class AboutOperators {
 		int i = 1;
 		int a = 6;
 		if ( (a < 9 ) ^ false)  i = i + 1;
-		assertEquals(i, __);
+		assertEquals(i, 2);
 	}
 	
 	@Koan
@@ -52,8 +52,8 @@ public class AboutOperators {
 		int i = 1;
 		boolean a = false;
 		if (a = true) i++;
-		assertEquals(a, __);
-		assertEquals(i, __);
+		assertEquals(a, true);
+		assertEquals(i, 2);
 		// How could you write the condition 'with a twist' to avoid this trap?
 	}
 	
@@ -61,24 +61,24 @@ public class AboutOperators {
 	public void aboutBitShiftingRightShift() {
 		int rightShift = 8;
 		rightShift = rightShift >> 1; 
-		assertEquals(rightShift, __);
+		assertEquals(rightShift, 4);
 	}
 	
 	@Koan
 	public void aboutBitShiftingLeftShift() {
 		int leftShift = 0x80000000; // Is this number positive or negative?
 		leftShift = leftShift << 1;
-		assertEquals(leftShift, __);
+		assertEquals(leftShift, 0);
 	}
 	
 	@Koan
 	public void aboutBitShiftingRightUnsigned() {
 		int rightShiftNegativeStaysNegative = 0x80000000;
 		rightShiftNegativeStaysNegative = rightShiftNegativeStaysNegative >> 4;
-		assertEquals(rightShiftNegativeStaysNegative, __);
+		assertEquals(rightShiftNegativeStaysNegative, -134217728);
 		int unsignedRightShift = 0x80000000; // always fills with 0
 		unsignedRightShift >>>= 4; // Just like +=
-		assertEquals(unsignedRightShift, __);
+		assertEquals(unsignedRightShift, 134217728);
 	}
 	
 }
